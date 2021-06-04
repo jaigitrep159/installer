3e88e6a9795af516177759a5d1a8f09dbd0270b4
Thunderzapper <justinabossmlg@gmail.com>
2021-06-04 15:11:20 -0400

    Update install_wrapper_offline.bat
    just incase we add submodules, this clones them

--- install_wrapper_offline.bat
+++ install_wrapper_offline.bat
@@ -179,7 +179,7 @@ title Wrapper: Offline Installer [Cloning repository...]
 pushd "%~dp0..\"
 echo Cloning the latest version of the repository from GitHub...
 echo:
-call git clone https://github.com/Wrapper-Offline/wrapper-offline.git
+call git clone https://github.com/Wrapper-Offline/wrapper-offline.git --recursive
 :: I'm doing this to get around the .gitignore problem but this is only for first-time users
 echo Resetting config.bat...
 del wrapper-offline\utilities\config.bat
